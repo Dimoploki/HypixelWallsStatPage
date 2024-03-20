2bf1ec4e60ff6c1e7c43cabe965c717bd108b79e
Dimo <dimiitris.dou@gmail.com>
2024-03-20 13:48:19 +0200

    made basic app

---
+++ index.js
@@ -0,0 +1,38 @@
+window.addEventListener(
+	'load', 
+	() => {
+		document.getElementById("search-button").addEventListener("click", lookup);
+		document.getElementById('remove-Api-key').addEventListener("click", 
+			()=> localStorage.removeItem('apiKey'));
+	}, false);
+	
+
+async function lookup() {
+	if (!localStorage.getItem("apiKey")) {
+		let key = prompt("Add your hypixel api key");
+		if(confirm("Save key for all future api calls?")) localStorage.setItem('apiKey', key);
+	}
+	const searchValue = document.getElementById('searchbar').value;
+	if (searchValue == '') return alert("Add a name maybe???");
+	const stats = await(await fetch(`https://api.hypixel.net/v2/player?name=${searchValue}`, { method: "GET", headers: {
+        "API-Key": localStorage.getItem("apiKey"),
+      },},
+ 	)).json();
+ 	if (!stats.success) return alert("Anilist API call did not suceed");
+ 	console.log(stats.player)
+ 	let walls = stats?.player?.stats?.Walls;
+ 	if (!walls) walls = {};
+ 	const { kills, wins, assists, losses, deaths } = walls;
+ 	const new_elem = `
+ 		<div class="stats">
+ 			Wins: ${wins || 0}<br>
+ 			Kills: ${kills || 0}<br>
+ 			Assists: ${assists || 0}<br>
+ 			Losses: ${losses || 0}<br>
+ 			Deaths: ${deaths || 0}<br>
+ 			K/D: ${Math.round((kills || 0)/(deaths || 0)*100)/100}<br>
+ 			W/L: ${Math.round((wins || 0)/(losses || 0)*100)/100}
+ 		</div>
+ 	`
+ 	document.getElementById("results").innerHTML = new_elem;
+}
